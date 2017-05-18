c38863c07a0d25955969c2f0c420a297169db1de
Sebastian Laskawiec <slaskawi@redhat.com>
2017-05-18 15:20:07 +0200

    ns

--- 2017_jug_bydgoszcz/antifroud-checker/src/main/java/org/infinispan/microservices/pipelines/GridToGridPipeline.java
+++ 2017_jug_bydgoszcz/antifroud-checker/src/main/java/org/infinispan/microservices/pipelines/GridToGridPipeline.java
@@ -1,11 +1,9 @@
 package org.infinispan.microservices.pipelines;
 
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 import org.infinispan.microservices.antifraud.service.AntiFraudQueryProcessor;
 import org.infinispan.microservices.caching.CacheInspector;
-import org.infinispan.microservices.transactions.model.Transaction;
 import org.infinispan.microservices.transactions.service.AntiFraudResultSender;
 import org.infinispan.microservices.transactions.service.AntifraudQueryMapper;
 import org.infinispan.microservices.transactions.service.AsyncTransactionReceiver;
@@ -32,8 +30,8 @@ public class GridToGridPipeline {
 
    public void processData() throws InterruptedException, ExecutionException {
       while(true) {
-         CompletableFuture<Transaction> take = transactionReceiver.getTransactionQueue().take();
-               take.thenApply(transaction -> queryMapper.toAntiFraudQuery(transaction))
+         transactionReceiver.getTransactionQueue().take()
+               .thenApply(transaction -> queryMapper.toAntiFraudQuery(transaction))
                .thenApply(antiFraudQueryData -> processor.process(antiFraudQueryData))
                .thenAccept(antiFraudResponseData -> {
                   cacheInspector.printOutCacheContent();
